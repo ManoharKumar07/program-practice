29424049b0f6e3f99f1832d9f1c1adb0af3a7143
Manohar Kumar <manohar.csengineer@gmail.com>
2024-10-14 22:19:23 +0530

    Wrapper class Initialisation

---
+++ WrapperInitusingConstructor.java
@@ -0,0 +1,36 @@
+
+class WrapperInitusingConstructor {
+
+    public static void main(String[] args) {
+        // Byte byteObj = new Byte((byte) 10);
+        // Short shortObj = new Short((short) 100);
+        // Integer intObj = new Integer(1000);
+        // Long longObj = new Long(100000L);
+        // Float floatObj = new Float(10.5f);
+        // Double doubleObj = new Double(20.99);
+        // Character charObj = new Character('A');
+        // Boolean boolObj = new Boolean(true);
+
+// // Using valueOf() method
+// Byte byteObj = Byte.valueOf((byte) 10);
+// Short shortObj = Short.valueOf((short) 100);
+// Integer intObj = Integer.valueOf(1000);
+// Long longObj = Long.valueOf(100000L);
+// Float floatObj = Float.valueOf(10.5f);
+// Double doubleObj = Double.valueOf(20.99);
+// Character charObj = Character.valueOf('A');
+// Boolean boolObj = Boolean.valueOf(true);
+
+// // Using autoboxing
+// Byte byteObj2 = 10;
+// Short shortObj2 = 100;
+// Integer intObj2 = 1000;
+// Long longObj2 = 100000L;
+// Float floatObj2 = 10.5f;
+// Double doubleObj2 = 20.99;
+// Character charObj2 = 'A';
+// Boolean boolObj2 = true;
+
+
+    }
+}
