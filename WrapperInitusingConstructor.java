785ea7cf1f2f1bb9df1dc71dcfbd9016c115d055
Manohar Kumar <manohar.csengineer@gmail.com>
2024-10-23 22:23:15 +0530

    string in c

--- WrapperInitusingConstructor.java
+++ WrapperInitusingConstructor.java
@@ -13,26 +13,60 @@ class WrapperInitusingConstructor {
         // Character charObj = new Character('A');
         // Boolean boolObj = new Boolean(true);
 
-// // Using valueOf() method
-// Byte byteObj = Byte.valueOf((byte) 10);
-// Short shortObj = Short.valueOf((short) 100);
-// Integer intObj = Integer.valueOf(1000);
-// Long longObj = Long.valueOf(100000L);
-// Float floatObj = Float.valueOf(10.5f);
-// Double doubleObj = Double.valueOf(20.99);
-// Character charObj = Character.valueOf('A');
-// Boolean boolObj = Boolean.valueOf(true);
+        // // Using valueOf() method
+        // Byte byteObj = Byte.valueOf((byte) 10);
+        // Short shortObj = Short.valueOf((short) 100);
+        // Integer intObj = Integer.valueOf(1000);
+        // Long longObj = Long.valueOf(100000L);
+        // Float floatObj = Float.valueOf(10.5f);
+        // Double doubleObj = Double.valueOf(20.99);
+        // Character charObj = Character.valueOf('A');
+        // Boolean boolObj = Boolean.valueOf(true);
 
-// // Using autoboxing
-// Byte byteObj2 = 10;
-// Short shortObj2 = 100;
-// Integer intObj2 = 1000;
-// Long longObj2 = 100000L;
-// Float floatObj2 = 10.5f;
-// Double doubleObj2 = 20.99;
-// Character charObj2 = 'A';
-// Boolean boolObj2 = true;
-
+        // // Using autoboxing
+        // Byte byteObj2 = 10;
+        // Short shortObj2 = 100;
+        // Integer intObj2 = 1000;
+        // Long longObj2 = 100000L;
+        // Float floatObj2 = 10.5f;
+        // Double doubleObj2 = 20.99;
+        // Character charObj2 = 'A';
+        // Boolean boolObj2 = true;
 
     }
 }
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+// char str1[] = "Hello";
+// char str2[10];
+
+// // Copy str1 to str2
+// strcpy(str2, str1);
+// printf("Copied string: %s\n", str2);
+
+// // Concatenate str1 and str2
+// strcat(str2, " World");
+// printf("Concatenated string: %s\n", str2);
+
+// // Length of str2
+// printf("Length of str2: %zu\n", strlen(str2));
+
+// // Compare str1 and str2
+// if (strcmp(str1, str2) == 0) {
+// printf("str1 and str2 are equal\n");
+// } else {
+// printf("str1 and str2 are not equal\n");
+// }
+
+// // Find character 'o' in str2
+// char *ptr = strchr(str2, 'o');
+// if (ptr != NULL) {
+// printf("Found 'o' at position: %ld\n", ptr - str2);
+// } else {
+// printf("'o' not found in str2\n");
+// }
+
+// return 0;
+// }
