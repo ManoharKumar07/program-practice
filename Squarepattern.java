632f38af844cd3bfce4f45146d2afa2ed4a007fe
Manohar Kumar <manohar.csengineer@gmail.com>
2025-02-07 10:59:27 +0530

    square pattern

---
+++ Squarepattern.java
@@ -0,0 +1,13 @@
+class Squarepattern{
+    public static void main(String args[]){
+        for (int i=0;i<4;i++){
+            for(int j=0;j<4-i-1;j++){
+                System.out.print("  ");
+            }
+            for (int j=0;j<i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
